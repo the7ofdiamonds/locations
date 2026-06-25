32ccef83ef9e6722d188bc72f355864815a21c90
jamelclyons <jamel.c.lyons@gmail.com>
2026-06-25 14:14:29 -0400

    Adding changes before iteration

--- src/views/location/Location.tsx
+++ src/views/location/Location.tsx
@@ -3,9 +3,9 @@ import { useState, useMemo } from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import type { LatLon } from 'use-places-autocomplete';
 
-import { PlacesAutocomplete } from '@/views/components/PlacesAutocomplete';
+import { PlacesAutocomplete } from '../components/PlacesAutocomplete';
 
-import { googleMapsAPIKey } from '@/services/google/Config';
+import { googleMapsAPIKey } from '../../services/google/Config';
 
 export const Location: React.FC = () => {
   const { isLoaded } = useLoadScript({
